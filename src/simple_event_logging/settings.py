bdd31eeb84906cd413a37124bf0b52c1648fccc0
mjlabe <mjlabe@gmail.com>
2024-03-27 13:55:54 -0400

    change data folder for db

--- src/simple_event_logging/settings.py
+++ src/simple_event_logging/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "simple_event_logging.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "data" / "db.sqlite3",
     }
 }
 
